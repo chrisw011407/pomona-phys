5708e762726fe2e83076cbff7e6643cd774f3b61
c <neutralizd@gmail.com>
2024-03-25 00:36:55 -0700

    modified styles.css

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 window.addEventListener('scroll', function () {
     const welcomeSection = document.querySelector('.welcome-section');
-    const header = document.querySelector('header');
     const main = document.querySelector('main');
 
-    if (window.scrollY > welcomeSection.offsetHeight / 2) {
-        welcomeSection.style.display = 'none';
-        header.style.display = 'block';
-        main.style.display = 'block';
+    if (window.scrollY > 0) {
+        welcomeSection.style.display = 'hidden';
+        main.style.display = 'visible';
     } else {
-        welcomeSection.style.display = 'flex';
-        header.style.display = 'none';
-        main.style.display = 'none';
+        welcomeSection.style.display = 'visible';
+        main.style.display = 'hidden';
     }
 });
+
